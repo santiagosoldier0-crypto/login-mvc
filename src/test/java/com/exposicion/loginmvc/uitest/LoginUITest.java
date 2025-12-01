7065a9dcc45eb81326d24a8314b3fe0848a5e989
ldalomoto <ldalomoto@uce.edu.ec>
2025-11-30 19:19:31 -0500

    selenium 3

--- src/test/java/com/exposicion/loginmvc/uitest/LoginUITest.java
+++ src/test/java/com/exposicion/loginmvc/uitest/LoginUITest.java
@@ -1,6 +1,6 @@
 package com.exposicion.loginmvc.uitest;
 
-import java.net.URL;
+import java.net.URL; 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
